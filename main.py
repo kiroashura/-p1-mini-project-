0d5bd23b898a5b0662bec9ccbfc8f7ed8b4d6b30
Johnwarlie67 <johnwarlieconcepcion147@gmail.com>
2026-08-28 09:23:59 +0000

    Add Python sum program

---
+++ main.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+total = num1 + num2 + num3
+print("The sum of the three numbers is:", total)
